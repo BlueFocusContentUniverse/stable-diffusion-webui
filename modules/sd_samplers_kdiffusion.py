e371e8726fcaa81dd9f03919d7cb1e212cde7050
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2023-05-22 23:26:28 +0800

    Replace karras by k_diffusion, fix gen info

--- modules/sd_samplers_kdiffusion.py
+++ modules/sd_samplers_kdiffusion.py
@@ -322,12 +322,16 @@ class KDiffusionSampler:
 
         if p.sampler_noise_scheduler_override:
             sigmas = p.sampler_noise_scheduler_override(steps)
-        elif p.enable_karras:
-            sigma_max = p.sigma_max
-            sigma_min = p.sigma_min
-            rho = p.rho
-            print(f"\nsigma_min: {sigma_min}, sigma_max: {sigma_max}, rho: {rho}")
-            sigmas = k_diffusion.sampling.get_sigmas_karras(n=steps, sigma_min=sigma_min, sigma_max=sigma_max, rho=rho, device=shared.device)
+        elif p.enable_custom_k_sched:
+            print(p.k_sched_type, p.sigma_min, p.sigma_max, p.rho)
+            sigmas_func = k_diffusion_scheduler[p.k_sched_type]
+            sigmas_kwargs = {
+                'sigma_min': p.sigma_min,
+                'sigma_max': p.sigma_max
+            }
+            if p.k_sched_type != 'exponential':
+                sigmas_kwargs['rho'] = p.rho
+            sigmas = sigmas_func(n=steps, **sigmas_kwargs, device=shared.device)
         elif self.config is not None and self.config.options.get('scheduler', None) == 'karras':
             sigma_min, sigma_max = (0.1, 10) if opts.use_old_karras_scheduler_sigmas else (self.model_wrap.sigmas[0].item(), self.model_wrap.sigmas[-1].item())
 
